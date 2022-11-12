a23d4f286eff0dc4c23ebb6008c04048560a2956
PrasadMutnale-exe <prasadmutnale1234@gmail.com>
2022-11-12 16:59:48 +0530

    src files are added

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Temp from './weather/Temp';
+
+function App() {
+  return (
+    <div className="App">
+      <Temp/>
+    </div>
+  );
+}
+
+export default App;
